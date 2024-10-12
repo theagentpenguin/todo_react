e58c09771ac54ab695d8d75ae11cf5ceece74d26
theagentpenguin <gunaseelanabcd@gmail.com>
2024-10-12 17:39:23 +0530

    turned the project into a react one

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <div class="main">
+            <p>Sample</p>
+            <div class="tasks">
+        
+            </div>
+            <div>
+                <button>add</button>
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default App;
